8d7ca05de5e7940c0d421800065bd9d84790bd7a
Piyathida Thaneewan <piyathida.thaneewan@mail.kmutt.ac.th>
2021-11-06 12:21:51 +0700

    successfulAuthentication

--- userservice/src/main/java/io/getarrays/userservice/filter/CustomAuthenticationFilter.java
+++ userservice/src/main/java/io/getarrays/userservice/filter/CustomAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package io.getarrays.userservice.filter;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,7 +13,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,27 +26,42 @@ import lombok.extern.slf4j.Slf4j;
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
 	private final AuthenticationManager authenticationManager;
-	
+
 	public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
 		this.authenticationManager = authenticationManager;
 	}
-	
+
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		log.info("Username is: {}",username);
-		log.info("Password is: {}",password);
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+		log.info("Username is: {}", username);
+		log.info("Password is: {}", password);
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+				password);
 		return authenticationManager.authenticate(authenticationToken);
 	}
 
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-			Authentication authResult) throws IOException, ServletException {
-		super.successfulAuthentication(request, response, chain, authResult);
+			Authentication authentication) throws IOException, ServletException {
+		User user = (User) authentication.getPrincipal();
+		Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+		String access_token = JWT.create()
+				.withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis()+10*60*1000))
+				.withIssuer(request.getRequestURI().toString())
+				.withClaim("role", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+				.sign(algorithm);
+		String refreash_token = JWT.create()
+				.withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis()+30*60*1000))
+				.withIssuer(request.getRequestURI().toString())
+				.sign(algorithm);
+		response.setHeader(access_token, access_token);
+		response.setHeader(refreash_token, refreash_token);
+		
 	}
 
-	
 }
